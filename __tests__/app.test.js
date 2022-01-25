858059bade2667300177b22b89631e925414367a
Katie Schrattenholzer <ktschr11@gmail.com>
2022-01-25 15:48:19 -0800

    cleanup routes

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,13 +2,19 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const Character = require('../lib/models/Character.js');
 
 describe('backend routes', () => {
   beforeEach(() => {
-    return setup(pool);
-  });
+    return setup(pool)
+  })
 
   afterAll(() => {
-    pool.end();
-  });
+    pool.end()
+  })
+
+  it('creates a character', async() => {
+    const res = await request(app)
+      .post('/characters')
+  })
 });
