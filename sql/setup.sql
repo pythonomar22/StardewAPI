7ce63ef5bd1a6129c43713c7356a781d5097f519
Katie Schrattenholzer <ktschr11@gmail.com>
2022-01-25 11:43:55 -0800

    sql table setup

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,11 +1,9 @@
--- Use this file to define your SQL tables
--- The SQL in this file will be executed when you run `npm run setup-db`
-DROP IF TABLE EXITSTS characters;
+DROP TABLE IF EXISTS characters;
 
 CREATE TABLE characters (
   id BIGINT GENERATED ALWAYS AS IDENTITY,
   name TEXT NOT NULL, 
   birthday TEXT NOT NULL,
   address TEXT NOT NULL,
-  elligible TEXT NOT NULL,
-)
+  elligible TEXT NOT NULL
+);
