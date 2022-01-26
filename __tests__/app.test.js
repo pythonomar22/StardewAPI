68d238569369d0d2dda18fee9839d7a0a6058a97
Katie Schrattenholzer <ktschr11@gmail.com>
2022-01-25 17:27:33 -0800

    added tests for create, get all, and get by id routes

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -13,19 +13,69 @@ describe('backend routes', () => {
     pool.end()
   })
 
-  it('creates a character', async() => {
-    const testChar = {
-      "name": "TestName",
-      "birthday": "Test 66",
-      "address": "666 Satan Road",
-      "elligible": "true",
-      "img": "https://blessthismess.com/img/666777888999.png"
+  const testChar1 = {
+      "name": "The Dark Lord",
+      "birthday": "Winter 6",
+      "address": "666 Highway to Hell",
+      "elligible": "very",
+      "img": "https://darklord.com/img/666.png"
     }
 
+  const testChar2 = {
+    "name": "Assistant to the Dark Lord",
+    "birthday": "Summer 7",
+    "address": "777 Light Ln.",
+    "elligible": "false",
+    "img": "https://darklord.com/img/777.png"
+  }
+
+  const testChar3 = {
+    "name": "Assistant to the Regional Dark Lord Assistant",
+    "birthday": "Fall 4",
+    "address": "777 Light Ln.",
+    "elligible": "true",
+    "img": "https://darklord.com/img/333.png"
+  }
+
+  const testArr = [{...testChar1, id: '1'}, {...testChar2, id: '2'}, {...testChar3, id: '3'}]
+
+  it('creates a character', async() => {
     const res = await request(app)
       .post('/api/v1/characters')
-      .send(testChar)
+      .send(testChar1)
 
-      expect(res.body).toEqual({...testChar, id: '1'})
+      expect(res.body).toEqual({...testChar1, id: '1'})
+
+      await Character.deleteById(res.body.id)
   })
+
+  it('gets one character by ID', async() => {
+    const char = await Character.insert(testChar1)
+
+    const res = await request(app)
+    .get(`/api/v1/characters/${char.id}`)
+
+    expect(res.body).toEqual({...testChar1, id: '1'})
+
+    await Character.deleteById(char.id)
+  })
+
+  it('gets all characters', async() => {
+    await Character.insert(testChar1)
+    await Character.insert(testChar2)
+    await Character.insert(testChar3)
+
+    const res = await request(app)
+    .get(`/api/v1/characters`)
+
+    expect(res.body).toEqual(testArr)
+  })
+
+  // it.only('updates a character', async() => {
+  //   const char = await Character.insert(testChar1)
+  //   const res = await request(app)
+  //   .patch(`/api/v1/characters/${char.id}`)
+
+  //   expect(res.body).toEqual([{...testChar1, id: '1'}, {...testChar2, id: '2'}, {...testChar3, id: '3'}])
+  // })
 });
