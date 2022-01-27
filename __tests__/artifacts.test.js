643213cb41124ab1ad3888600875f16578babf8a
Katie Schrattenholzer <ktschr11@gmail.com>
2022-01-27 10:30:08 -0800

    get all and by ID tests added

--- __tests__/artifacts.test.js
+++ __tests__/artifacts.test.js
@@ -35,4 +35,49 @@ describe('backend routes', () => {
 
       // await Seed.deleteById(res.body.id)
   })
+
+  it('should get all artifacts', async() => {
+
+    const artifact = await Artifact.insert({
+      name: "Jazzy Artifact",
+      abt: "Alll about this jazzy artifact",
+      sell_price: "15g",
+      img: 'https://stardewvalleywiki.com/mediawiki/images/9/9e/Chipped_Amphora.png'
+    })
+
+    const seedData = {
+          name: "Chipped Amphora",
+          abt: "An ancient vessel made of ceramic material. Used to transport both dry and wet goods.",
+          sell_price: "40g",
+          img: 'https://stardewvalleywiki.com/mediawiki/images/9/9e/Chipped_Amphora.png'
+        }
+    
+    const res = await request(app).get('/api/v1/artifacts')
+
+    const artifactList = [
+      {...seedData, id: expect.any(String)},
+      {...artifact, id: expect.any(String)}
+    ]
+
+    expect(res.body).toEqual(artifactList)
+  })
+
+  it('should get one artifact by ID', async() => {
+    const artifact = await Artifact.insert({
+      name: "Jazzy Artifact",
+      abt: "Alll about this jazzy artifact",
+      sell_price: "15g",
+      img: 'https://stardewvalleywiki.com/mediawiki/images/9/9e/Chipped_Amphora.png'
+    })
+    const res = await request(app).get(`/api/v1/artifacts/${artifact.id}`)
+    const expectation = {
+      id: expect.any(String),
+        name: "Jazzy Artifact",
+        abt: "Alll about this jazzy artifact",
+        sell_price: "15g",
+        img: 'https://stardewvalleywiki.com/mediawiki/images/9/9e/Chipped_Amphora.png'
+    }
+
+    expect(res.body).toEqual(expectation)
+  })
 });
